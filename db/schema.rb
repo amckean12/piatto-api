75fd0822e280fee6a3d28a5b2670297f4e488019
amckean12 <amckean@me.com>
2019-05-23 08:17:34 -0500

    Added seed files

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_195612) do
+ActiveRecord::Schema.define(version: 2019_05_23_125731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "password_digest"
+  create_table "ingredients", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.create(name: "Tomato Sauce with Onion Butter", description: "This is the simplest of all sauces to make, and none has a purer, more irresistibly sweet tomato taste. I have known people to skip the pasta and eat the sauce directly out of the pot with a spoon.")
+Ingredient.create(description: "2 pounds fresh, ripe tomatoes") #0
+Ingredient.create(description: "1 medium onion, peeled and cut in half") #1
+Ingredient.create(description: "5 tablespoons of butter") #2
+Ingredient.create(description: "Salt") #3
+Ingredient.create(description: "1 to 1.5 pounds of pasta") #4
+Ingredient.create(description: "Freshly grated parmigianoreggiano cheese for the table") #5
+
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 0)
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 1)
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 2)
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 3)
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 4)
+RecipeIngredient.create(recipe_id: 0, ingredient_id: 5)
