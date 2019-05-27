92e98abe024d86ea932ba18228d3f51accab9666
amckean12 <amckean@me.com>
2019-05-27 13:33:44 -0500

    Backend Auth working on post man

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,4 +1,5 @@
 class Api::RecipesController < ApplicationController
+  before_action :authenticate_user, only: [:auth]
   def index
     @recipes = Recipe.all
     render json: @recipes, status: 200
@@ -7,5 +8,9 @@ class Api::RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     render json: @recipe, status: 200
+  end
+
+  def auth
+    render json: {status: 200, msg: "You are currently logged in as #{current_user.email}"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    '/users'          => 'users#index'
   get    '/users/current'  => 'users#current'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   #Current Version
   namespace :api do
       resources :recipes, only: [:index, :show]
+      get '/auth' => "recipes#auth"
       resources :ingredients, only: [:index]
       resources :tags, only: [:show], param: :slug
   end
