bea203bd28c9b29d0f004e0ea8e50017cf3c5db2
amckean12 <amckean@me.com>
2019-05-28 11:10:33 -0500

    fixes the 422 with knock and rails 5.2

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,2 +1,4 @@
 class Api::UserTokenController < Knock::AuthTokenController
+  #Solves 422 issue with rails 5.2 and Knock
+  skip_before_action :verify_authenticity_token
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_180701) do
+ActiveRecord::Schema.define(version: 2019_05_28_154324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2019_05_23_180701) do
   create_table "tags", force: :cascade do |t|
     t.string "tag_name"
     t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
