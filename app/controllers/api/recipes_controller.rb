317043e9280d826a007eee9c562b0b0eaccff3c8
amckean12 <amckean@me.com>
2019-06-05 17:11:36 -0500

    Recipe can be created in the backend

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -8,8 +8,21 @@ class Api::RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    binding.pry
-    render json: @recipe, status: 200
+    @ingredients = recipe_params["ingredients_attributes"]
+    @tags = recipe_params["tags_attributes"]
+    @ingredients.each do |ingredient|
+      @recipe.ingredients.build(ingredient)
+    end
+
+    @tags.each do |tag|
+      @recipe.tags.build(tag)
+    end
+
+    if @recipe.save
+      render json: @recipe, status: 200
+    else
+      render json: @recipe.errors, status: :unprocessable_entry
+    end
   end
 
   def show
