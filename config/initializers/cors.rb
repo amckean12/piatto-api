fe2f0680048c770456cf21c7c8c722daab7fdb52
amckean12 <amckean@me.com>
2019-06-20 17:29:16 -0500

    took out url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
      allow do
-       origins 'https://piatto-client.herokuapp.com/'
+       origins '*'
        resource '*', headers: :any, methods: [:get, :post, :options]
      end
 end
