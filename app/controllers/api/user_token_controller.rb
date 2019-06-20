e8112ce320d529786c6051ffeca827d39bb65c8f
amckean12 <amckean@me.com>
2019-06-20 17:58:03 -0500

    trying this

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,4 +1,8 @@
 class Api::UserTokenController < Knock::AuthTokenController
   #Solves 422 issue with rails 5.2 and Knock
   skip_before_action :verify_authenticity_token
+
+  def create
+  end
+  
 end
