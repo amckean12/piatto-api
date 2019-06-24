9bf3eca3b2e50fbda12b45e7aab148809b42c332
amckean12 <amckean@me.com>
2019-06-24 10:42:36 -0500

    Solved duplicate tag issue

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,15 @@ class Recipe < ApplicationRecord
   has_many :tags, :through => :recipe_tags
 
   accepts_nested_attributes_for :ingredients, :tags, allow_destroy: true,  reject_if: lambda { |a| a[:content].blank? }
+
+  before_save :find_or_create_tag
+
+  private
+
+#Solved Duplicate Tag issue
+  def find_or_create_tag
+    self.tags = self.tags.map do |tag|
+      Tag.find_or_create_by(tag_name: tag.tag_name, slug: tag.slug)
+    end
+  end
 end
